845bac4b9d72076a485d81b21d2a927f31050013
dbsthf04 <84370143+dbsthf04@users.noreply.github.com>
2023-09-22 00:21:37 +0900

    Update 05_practice_2.ino.ino

--- 05_practice_2.ino.ino
+++ 05_practice_2.ino.ino
@@ -1,34 +1,28 @@
 #define PIN_LED 7
 unsigned int count, toggle;
 
-// 함수 원형 선언
 int toggle_state(int toggle);
 
 void setup() {
-  pinMode(PIN_LED, OUTPUT);  // LED 핀을 출력으로 설정
+  pinMode(PIN_LED, OUTPUT);  
   Serial.begin(115200);
   digitalWrite(PIN_LED,0);
   delay(1000);
   while(!Serial){
     ;
   }
-  
   Serial.println("Hello World!");
   count = toggle = 0;
-  
 }
 
 void loop() {
-   if (count < 10) { // count가 10보다 작을 때만 아래 코드를 실행
+   if (count < 10) {
     Serial.println(++count);
     toggle = toggle_state(toggle);
     digitalWrite(PIN_LED, toggle);
     delay(100);
   } else {
-    // count가 10 이상이면 루프를 멈춤
     digitalWrite(PIN_LED, 1);
-      // 루프를 멈추기 위해 아무것도 하지 않음
-  
   }
 }
 
